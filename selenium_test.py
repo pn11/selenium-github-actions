d0d57838c1f25d6202cd121c4ab0506f9b2606b9
oka <pn11@users.noreply.github.com>
2021-06-16 02:53:45 +0900

    add discord webhook

--- selenium_test.py
+++ selenium_test.py
@@ -1,8 +1,12 @@
+import json
 import os
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.common.exceptions import NoSuchElementException
+
+DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 
 chromeOptions = webdriver.ChromeOptions()
 chromeOptions._binary_location = '/usr/bin/google-chrome-stable'
@@ -14,6 +18,9 @@ chromeOptions.add_experimental_option("prefs", prefs)
 
 driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=chromeOptions)
 
-driver.get('https://google.com')
 
-print(driver.page_source)
+GOOGLE = 'https://www.google.com'
+driver.get(GOOGLE)
+img_url = GOOGLE + driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/img').get_attribute('src')
+
+r = requests.post(DISCORD_WEBHOOK_URL, data=json.dumps({'content': 'test', 'embeds': [{'image': {'url': img_url}}]}), headers={'content-type': 'application/json'})
